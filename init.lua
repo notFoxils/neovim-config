7fb4cdd31d43b639609930c3b3e8cfedbb27d54f
notFoxils <voltrixz9514@gmail.com>
2025-07-16 00:54:37 -0400

    gopls addition

--- init.lua
+++ init.lua
@@ -27,20 +27,7 @@ vim.schedule(function()
     vim.opt.clipboard = "unnamedplus"
 end)
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-    if client == nil then
-        return
-    end
-
-    if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-
 require("misc.lazy")
 
-vim.lsp.enable("lua-ls")
+vim.lsp.enable("gopls")
+vim.lsp.enable("lua_ls")
