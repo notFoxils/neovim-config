c5b6766be18cb1eec7545355d02d0ae2ac77c1fc
notFoxils <voltrixz9514@gmail.com>
2025-10-29 19:44:16 -0400

    feat: Added 'nvim-neo-tree/neo-tree.nvim'

--- lua/plugins/lsp-ext-lazydev.lua
+++ lua/plugins/lsp-ext-lazydev.lua
@@ -3,6 +3,7 @@ return {
     ft = "lua",
     opts = {
         library = {
+            "lazy.nvim",
             -- Load luvit types when the `vim.uv` word is found
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
--- lua/plugins/ui-neo-tree.lua
+++ lua/plugins/ui-neo-tree.lua
@@ -1,24 +1,5 @@
-local function setupKeybinds(bufferNumber)
-    require("nvim-tree.api").config.mappings.default_on_attach(bufferNumber)
-
-    local function opts(desc)
-        return {
-            desc = "nvim-tree: " .. desc,
-            -- buffer = bufferNumber,
-            noremap = true,
-            silent = true,
-            nowait = true,
-        }
-    end
-
-    vim.keymap.set("n", "<C-n>",     "<cmd>NvimTreeToggle<CR>", opts("Toggle Window"))
-    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>",  opts("Focus Window"))
-end
-
-local function onAttach(bufferNumber)
-    setupKeybinds(bufferNumber)
-end
-
+---@module "lazy.nvim"
+---@type LazyPluginSpec
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -27,9 +8,20 @@ return {
         "MunifTanjim/nui.nvim",
         "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
     ---@module 'neo-tree'
     ---@type neotree.Config
     opts = {
+        window = {
+            position = "right",
+        },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+            },
+        },
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    keys = {
+        { "<C-n>", "<cmd>Neotree toggle<CR>", mode = "n", desc = "" },
     },
 }
