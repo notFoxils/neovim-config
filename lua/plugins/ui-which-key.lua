12bf366c41489d7e18ee99882f83a680b7053caf
notFoxils <voltrixz9514@gmail.com>
2026-04-16 10:51:43 -0400

    style: Remove Useless Comments In `which-key.nvim` Configuration
    summary:
      - Remove comments remenant from `kickstart.nvim`.

--- lua/plugins/ui-which-key.lua
+++ lua/plugins/ui-which-key.lua
@@ -2,18 +2,12 @@
 ---@type LazyPluginSpec
 return {
     "folke/which-key.nvim",
-    event = "VeryLazy",
     ---@module "which-key"
     ---@type wk.Opts
     opts = {
-        -- delay between pressing a key and opening which-key (milliseconds)
-        -- this setting is independent of vim.opt.timeoutlen
         delay = 200,
         icons = {
-            -- set icon mappings to true if you have a Nerd Font
             mappings = vim.g.have_nerd_font,
-            -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-            -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
             keys = vim.g.have_nerd_font and {} or {
                 Up = "<Up> ",
                 Down = "<Down> ",
@@ -45,15 +39,6 @@ return {
                 F12 = "<F12>",
             },
         },
-        -- Document existing key chains
-        --[[spec = {
-            { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
-            { "<leader>d", group = "[D]ocument" },
-            { "<leader>r", group = "[R]ename" },
-            { "<leader>s", group = "[S]earch" },
-            { "<leader>w", group = "[W]orkspace" },
-            { "<leader>t", group = "[T]oggle" },
-            { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-        },]]
     },
+    event = "VeryLazy",
 }
